63659cda7cd2004cad0fef161c840582da0e85d2
Carmen Peralta <cperalta26@gmail.com>
2018-11-13 17:51:58 -0500

    add client routes

---
+++ client/appRoutes.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react"
+import { Route, Switch } from "react-router-dom"
+import {
+  Home,
+  BuildingsListContainer,
+  BuildingContainer,
+  BuildingRentalListContainer,
+  BuildingRentalUnitContainer,
+  AllRentalUnitsContainer
+} from "./components"
+
+import "../public/index.css"
+
+export default class App extends Component() {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/buildings' component={BuildingsListContainer} />
+        <Route path='/buildings/:buildingId/' component={BuildingContainer} />
+        <Route path='/buildings/:buildingId/rentalUnitsInBuilding' component={BuildingRentalListContainer}/>
+        <Route path='/buildings/:buildingId/rentalUnitsInBuilding/:rentalUnitId' component={BuildingRentalUnitContainer} />
+        <Route path='allunits' component={AllRentalUnitsContainer} />
+      </Switch>
+    )
+  }
+}
--- client/index.js
+++ client/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import App from './components/App';
+import React from 'react'
+import ReactDom from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
+import App from './appRoutes'
 
 ReactDom.render(
   <Router>
     <App />
   </Router>,
   document.getElementById('rental-management-app'),
-);
+)
