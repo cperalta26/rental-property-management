85b0f4f19fea343cf2566e6ebad60a7ca857ee1e
Carmen Peralta <cperalta26@gmail.com>
2018-12-29 18:52:52 -0500

    add initial css to home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { homePage } from '../../public/index.css'
 
-const Home = () => <h1>Home!!!!</h1>
+const Home = () => (
+  <div className={homePage}>
+    <h1>Home Page</h1>
+  </div>
+)
 
 export default Home
