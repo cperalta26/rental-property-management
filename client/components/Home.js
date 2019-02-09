f235bc6ff0867518615ca4bb4662c69521c3778a
Carmen Peralta <cperalta26@gmail.com>
2019-02-09 17:41:26 -0500

    feat(home): add block

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { header, logo } from '../../public/index.css'
+import style from '../../public/index.css'
 import imageLogo  from '../../public/Images/companyLogo.png'
 
 const Home = () => (
-  <header className={ header }>
-    <div className={ logo }>
+  <header className={ style.header }>
+    <div className={ style.logo }>
       <img src={ imageLogo } alt="logo"/>
+      <div className={ `${style.blockOne} ${style.moveToRight} ${style.object}` }>x</div>
     </div>
   </header>
 )
